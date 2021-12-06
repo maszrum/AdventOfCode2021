92ea1e732083dea7a1c0c17bac9a88369b17e4ce
maszrum <grzyb3k@hotmail.com>
2021-12-06 12:14:08 +0100

    day 6

---
+++ src/AdventOfCode.Day06/Program.cs
@@ -0,0 +1,27 @@
+﻿var reader = new InputFileReader("input.txt");
+
+var input = (await reader.ReadAllLines())[0];
+
+var lanternFish = input
+    .Split(',')
+    .Select(int.Parse)
+    .GroupBy(
+        internalTimer => internalTimer, 
+        (internalTimer, fish) => (InternalTimer: internalTimer, Count: fish.LongCount()))
+    .ToDictionary(g => g.InternalTimer, g => g.Count);
+
+var school = new SchoolOfLanternFish(lanternFish);
+
+for (var day = 1; day <= 80; day++)
+{
+    school.NextDay();
+}
+
+Console.WriteLine($"After 80 days, there would be {school.Count()} lantern fish.");
+
+for (var day = 81; day <= 256; day++)
+{
+    school.NextDay();
+}
+
+Console.WriteLine($"After 256 days, there would be {school.Count()} lantern fish.");
---
+++ src/AdventOfCode.Day06/SchoolOfLanternFish.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Day06;
+
+internal class SchoolOfLanternFish
+{
+    private readonly Dictionary<int, long> _count;
+    
+    public SchoolOfLanternFish(Dictionary<int, long> count)
+    {
+        _count = count.ToDictionary(k => k.Key, v => v.Value);
+        
+        for (var i = 0; i <= 8; i++)
+        {
+            if (!_count.ContainsKey(i))
+            {
+                _count.Add(i, 0);
+            }
+        }
+    }
+    
+    public void NextDay()
+    {
+        var zeroInternalTimerFish = _count[0];
+        
+        for (var i = 1; i <= 8; i++)
+        {
+            _count[i - 1] = _count[i];
+        }
+        _count[6] += zeroInternalTimerFish;
+        _count[8] = zeroInternalTimerFish;
+    }
+    
+    public long Count() => _count.Values.Sum();
+}
