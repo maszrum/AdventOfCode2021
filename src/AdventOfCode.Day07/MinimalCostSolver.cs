9e53aa8b768a77dcb7e5642c5500aa35b5a91c47
maszrum <grzyb3k@hotmail.com>
2021-12-07 09:20:56 +0100

    day 7

---
+++ src/AdventOfCode.Day07/MinimalCostSolver.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode.Day07;
+
+internal class MinimalCostSolver
+{
+    private readonly Func<int, int, int> _costFunction;
+
+    public MinimalCostSolver(Func<int, int, int> costFunction)
+    {
+        _costFunction = costFunction;
+    }
+
+    public int FindMinimalCost(IReadOnlyList<int> positions)
+    {
+        var min = positions.Min();
+        var max = positions.Max();
+    
+        var minimalCost = int.MaxValue;
+
+        for (var position = min; position <= max; position++)
+        {
+            var cost = positions
+                .Sum(p => _costFunction(p, position));
+        
+            minimalCost = Math.Min(minimalCost, cost);
+        }
+    
+        return minimalCost;
+    }
+}
---
+++ src/AdventOfCode.Day07/Program.cs
@@ -0,0 +1,32 @@
+﻿// ReSharper disable AccessToModifiedClosure
+
+var reader = new InputFileReader("input.txt");
+
+var crabPositions = (await reader.ReadFirstLine())
+    .Split(',')
+    .Select(int.Parse)
+    .ToArray();
+
+/*
+ * Part one
+ */
+
+var solverOne = new MinimalCostSolver(
+    costFunction: (from, to) => Math.Abs(from - to));
+
+var fuelPartOne = solverOne.FindMinimalCost(crabPositions);
+
+Console.WriteLine($"The crabs must spend {fuelPartOne} fuel.");
+
+/*
+ * Part two
+ */
+
+int SumOfArithmeticSequence(int n) => (n + n * n) / 2;
+
+var solverTwo = new MinimalCostSolver(
+    costFunction: (from, to) => SumOfArithmeticSequence(Math.Abs(from - to)));
+
+var fuelPartTwo = solverTwo.FindMinimalCost(crabPositions);
+
+Console.WriteLine($"The crabs must spend {fuelPartTwo} fuel.");
